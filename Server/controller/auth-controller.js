e30d5126edafee41e392d7a576dbd2ee87185c7b
Chamika Jayarathna <failgames.lk@gmail.com>
2025-01-07 03:07:45 +0530

    create logout route

--- Server/controller/auth-controller.js
+++ Server/controller/auth-controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import 'dotenv/config';
+import "dotenv/config";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
@@ -28,33 +28,41 @@ export const login = async (req, res) => {
   const age = 1000 * 60 * 60 * 24 * 7;
 
   try {
-
     const user = await prisma.user.findUnique({
-      where:{ username }
+      where: { username },
     });
-    
-    if(!user) {return res.status(401).json({message: "Invalid Credentials!"});}
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Credentials!" });
+    }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordValid) {return res.status(401).json({message: "Invalid Credentials!"});}
-    
-    const token = jwt.sign({
-      id: user.id,
-    }, process.env.JWT_SECRET_KEY, {expiresIn:age});
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid Credentials!" });
+    }
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      maxAge: age,
-    })
-    .status(200)
-    .json({ message: "Login Successful" });
-    
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "Login Successful" });
   } catch (error) {
-    res.status(500).json({message: "Failed to login!"});
+    res.status(500).json({ message: "Failed to login!" });
   }
-
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res.clearCookie("token").status(200).json({message: "Logout Successful"});
+};
