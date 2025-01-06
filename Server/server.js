25eb5eab9179e84ffb4213e993cba83c3515a473
Chamika Jayarathna <failgames.lk@gmail.com>
2025-01-06 09:47:46 +0530

    install prisma and update other files

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import authRoute from './routes/auth-route.js';
 import postRoute from './routes/post-route.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 
+app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
