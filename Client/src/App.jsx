e963620ca59b3f3a16c2eee344d7e20ba5afe0e3
Chamika Jayarathna <failgames.lk@gmail.com>
2025-01-08 12:53:03 +0530

    update home page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './layout.scss';
 import NavBar from './components/NavBar/NavBar';
+import HomePage from './pages/HomePage/HomePage';
 
 const App = () => {
   return (
     <div className="layout">
-      <NavBar/>
+      <div className="navbar">
+        <NavBar/>
+      </div>
+      <div className="content">
+        <HomePage/>
+      </div>
     </div>
   )
 }
---
+++ Client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './HomePage.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+        <div className="textContainer">
+            <div className="wrapper">
+                <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+                    explicabo suscipit cum eius, iure est nulla animi consequatur
+                    facilis id pariatur fugit quos laudantium temporibus dolor ea
+                    repellat provident impedit!
+                </p>
+            </div>
+        </div>
+
+        <div className="imgContainer">
+            <img src="/bg.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default HomePage;
