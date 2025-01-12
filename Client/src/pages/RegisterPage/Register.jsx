501b96044d44a35811264e0a3e4d66920ffcce8f
Chamika Jayarathna <failgames.lk@gmail.com>
2025-01-12 14:34:05 +0530

    create Register page registretion logic

--- Client/src/pages/RegisterPage/Register.jsx
+++ Client/src/pages/RegisterPage/Register.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import "./Register.scss";
 
 const Register = () => {
+
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      console.log(res.data);
+      
+      
+    } catch (error) {
+      console.log(error);
+    }
+
+
+  }
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import "dotenv/config";
 import authRoute from './routes/auth-route.js';
 import postRoute from './routes/post-route.js';
 
@@ -8,6 +10,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
