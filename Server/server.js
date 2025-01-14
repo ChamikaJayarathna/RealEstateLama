d231181fc62a55eaede93dc87d68bdbcc998a23b
Chamika Jayarathna <failgames.lk@gmail.com>
2025-01-14 13:05:03 +0530

    create user controller logics

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ import "dotenv/config";
 import authRoute from './routes/auth-route.js';
 import postRoute from './routes/post-route.js';
 import testRoute from './routes/test-route.js';
+import userRoute from './routes/user-route.js';
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/test', testRoute);
 
