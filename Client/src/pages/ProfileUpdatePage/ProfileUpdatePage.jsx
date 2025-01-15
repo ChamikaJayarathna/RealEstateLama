c69365a950cbf9939162229e0fb789c8a2aa4288
Chamika Jayarathna <failgames.lk@gmail.com>
2025-01-15 17:58:40 +0530

    Create React Upload Image Function

--- Client/src/pages/ProfileUpdatePage/ProfileUpdatePage.jsx
+++ Client/src/pages/ProfileUpdatePage/ProfileUpdatePage.jsx
@@ -1,34 +1,35 @@
-import apiRequest from "../../lib/apiRequest";
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
 import { useNavigate } from "react-router-dom";
+import UploadWidget from "../../components/UploadWidget/UploadWidget";
 import "./ProfileUpdatePage.scss";
 
-const ProfileUpdatePage = () => {
-  const [error, setError] = useState("");
-
+function ProfileUpdatePage() {
   const { currentUser, updateUser } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [avatar, setAvatar] = useState([]);
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const formData = new FormData(e.target);
+
     const { username, email, password } = Object.fromEntries(formData);
 
     try {
-      const res = await apiRequest.put(`/users/${currentUser.id}`,
-        {
-          username,
-          email,
-          password,
-        }
-      );
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+        avatar: avatar[0],
+      });
       updateUser(res.data);
-      navigate('/profile');
-    } catch (error) {
-      console.log(error);
-      setError(error.response.data.message);
+      navigate("/profile");
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.message);
     }
   };
 
@@ -65,13 +66,23 @@ const ProfileUpdatePage = () => {
       </div>
       <div className="sideContainer">
         <img
-          src={currentUser.avatar || "./noavatar.jpg"}
+          src={avatar[0] || currentUser.avatar || "/noavatar.jpg"}
           alt=""
           className="avatar"
+        />
+        <UploadWidget
+          uwConfig={{
+            cloudName: "diptmwpyy",
+            uploadPreset: "estate",
+            multiple: false,
+            maxImageFileSize: 2000000,
+            folder: "avatars",
+          }}
+          setState={setAvatar}
         />
       </div>
     </div>
   );
-};
+}
 
 export default ProfileUpdatePage;
